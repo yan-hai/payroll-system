319b0e1b2a30be663b131ebf8211aeb3cf107a40
javis-yan <yan_h@hotmail.com>
2018-05-06 11:58:31 +0800

    [calc-core]Proration Formula

---
+++ calc-core/src/main/java/com/nobodyhub/payroll/core/formula/proration/ProrationFormula.java
@@ -0,0 +1,38 @@
+package com.nobodyhub.payroll.core.formula.proration;
+
+import com.google.common.collect.Sets;
+import com.nobodyhub.payroll.core.exception.PayrollCoreException;
+import com.nobodyhub.payroll.core.formula.common.Formula;
+import com.nobodyhub.payroll.core.item.ItemContext;
+import com.nobodyhub.payroll.core.item.common.Item;
+import com.nobodyhub.payroll.core.item.payment.PaymentItem;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+/**
+ * @author Ryan
+ */
+public class ProrationFormula extends Formula {
+    private String partItemId;
+    private String totalItemId;
+    private String paymentItemId;
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public PaymentItem evaluate(ItemContext context) throws PayrollCoreException {
+        Item<BigDecimal, ?> part = itemFactory.getItem(partItemId);
+        Item<BigDecimal, ?> total = itemFactory.getItem(totalItemId);
+        Item<BigDecimal, ?> payment = itemFactory.getItem(paymentItemId);
+
+        BigDecimal rst = part.getValue().multiply(payment.getValue()).divide(total.getValue(), context.getMathContext());
+        PaymentItem item = createPaymentItem();
+        item.setValue(rst);
+        return item;
+    }
+
+    @Override
+    public Set<String> getRequiredItems() {
+        return Sets.newHashSet(partItemId, totalItemId, paymentItemId);
+    }
+}
