cb0f05d6107b1fb2772268ff8acf4600e7c67c23
javis-yan <yan_h@hotmail.com>
2018-05-06 23:56:02 +0800

    [calc-core] add callback to Task

--- calc-core/src/main/java/com/nobodyhub/payroll/core/task/Task.java
+++ calc-core/src/main/java/com/nobodyhub/payroll/core/task/Task.java
@@ -1,8 +1,10 @@
 package com.nobodyhub.payroll.core.task;
 
 import com.nobodyhub.payroll.core.exception.PayrollCoreException;
+import com.nobodyhub.payroll.core.task.callback.Callback;
 import lombok.Getter;
 
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -15,21 +17,27 @@ import java.util.concurrent.Executors;
 public abstract class Task {
     protected String taskId;
     protected TaskContext taskContext;
-    private ExecutionCallback callback;
+    private Callback callback;
 
     private ExecutorService executorService = Executors.newFixedThreadPool(5);
 
-    public void beforeExec() {
+    public void setup() {
         taskContext.getFormulaContext().prioritize();
     }
 
-    public void execute(ExecutionContext executionContext) throws PayrollCoreException {
-        executionContext.setTaskContext(taskContext);
+    public void execute(Map<String, String> valueMap) throws PayrollCoreException {
+        ExecutionContext executionContext = createExecutionContext(valueMap);
         TaskExecution execution = new TaskExecution(executionContext, callback);
         executorService.execute(execution);
     }
 
-    public void afterExec() {
+    public void cleanup() {
         //empty implementation
+    }
+
+    private ExecutionContext createExecutionContext(Map<String, String> valueMap) throws PayrollCoreException {
+        ExecutionContext executionContext = new ExecutionContext(taskContext);
+        executionContext.addAll(valueMap);
+        return executionContext;
     }
 }
