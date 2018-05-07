1cd0d8b6d8feb3ddcdf5dfbfc9905080f2ab0bae
javis-yan <yan_h@hotmail.com>
2018-05-07 09:36:43 +0800

    [calc-core]add TaskManager#clear to clear from cache

--- calc-core/src/main/java/com/nobodyhub/payroll/core/task/TaskManager.java
+++ calc-core/src/main/java/com/nobodyhub/payroll/core/task/TaskManager.java
@@ -3,19 +3,30 @@ package com.nobodyhub.payroll.core.task;
 import com.google.common.collect.Maps;
 
 import java.util.Map;
+import java.util.Set;
 
 /**
+ * TODO: make it thread-safe
+ *
  * @author Ryan
  */
 public class TaskManager {
+    /**
+     * change to use google cache
+     */
     private Map<String, Task> tasks = Maps.newHashMap();
 
     public void register(Task task) {
         tasks.put(task.getTaskId(), task);
     }
 
-    public void get(String taskId) {
+    public Task get(String taskId) {
         //TODO: handle null case
-        tasks.get(taskId);
+        //TODO: if null, get from DB
+        return tasks.get(taskId);
+    }
+
+    public void clear(Set<String> taskIds) {
+        tasks.keySet().removeAll(taskIds);
     }
 }
