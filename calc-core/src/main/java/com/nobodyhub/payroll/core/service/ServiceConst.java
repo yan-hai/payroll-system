6fea4a131fee7eb964f84267e8530fa47089bec3
yan_h <yan_h@worksap.co.jp>
2018-05-07 13:51:14 +0800

    [calc-core]add ServiceConst

---
+++ calc-core/src/main/java/com/nobodyhub/payroll/core/service/ServiceConst.java
@@ -0,0 +1,12 @@
+package com.nobodyhub.payroll.core.service;
+
+/**
+ * @author yan_h
+ * @since 2018-05-07.
+ */
+public final class ServiceConst {
+    private ServiceConst() {
+    }
+
+    public static final int DEFAULT_PORT = 9101;
+}
