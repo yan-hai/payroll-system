dd057cc605422d49918cef6d844f44b6f3156a81
yan_h <yan_h@worksap.co.jp>
2018-05-10 13:17:19 +0800

    [calc-core]add PayrollCoreConst#END_OF_TIME

--- calc-core/src/main/java/com/nobodyhub/payroll/core/util/PayrollCoreConst.java
+++ calc-core/src/main/java/com/nobodyhub/payroll/core/util/PayrollCoreConst.java
@@ -3,6 +3,7 @@ package com.nobodyhub.payroll.core.util;
 import com.nobodyhub.payroll.core.item.payment.PaymentItem;
 
 import java.math.MathContext;
+import java.time.LocalDate;
 
 import static java.math.MathContext.DECIMAL128;
 
@@ -19,4 +20,6 @@ public final class PayrollCoreConst {
      * the actual for each item rounding rule will be decided by {@link PaymentItem#roundingRule}
      */
     public static final MathContext MATH_CONTEXT = DECIMAL128;
+
+    public static final LocalDate END_OF_TIME = LocalDate.of(2382, 12, 31);
 }
