1324cbf20285fd9297efbdbb41ce24be8fa068d3
yan_h <yan_h@worksap.co.jp>
2018-05-11 17:40:06 +0800

    [calc-core]remove abstract for Task

--- calc-core/src/main/java/com/nobodyhub/payroll/core/task/Task.java
+++ calc-core/src/main/java/com/nobodyhub/payroll/core/task/Task.java
@@ -14,6 +14,7 @@ import com.nobodyhub.payroll.core.task.execution.normal.NormalTaskExecution;
 import com.nobodyhub.payroll.core.task.execution.retro.HistoryData;
 import com.nobodyhub.payroll.core.task.execution.retro.RetroTaskExecution;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -25,7 +26,8 @@ import java.util.concurrent.Executors;
  * @author Ryan
  */
 @Data
-public abstract class Task implements Identifiable {
+@RequiredArgsConstructor
+public class Task implements Identifiable {
     /**
      * Task Id
      */
